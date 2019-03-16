6b6b22ca0d66523cba8b4a9d186d85e8ae08a804
jellyfish26 <jellyfish026@outlook.jp>
2019-03-17 01:42:37 +0900

    Resolve login bug

--- app/src/main/java/jellyfish026/trunkhackathon/HomeScreen.kt
+++ app/src/main/java/jellyfish026/trunkhackathon/HomeScreen.kt
@@ -1,2 +1,13 @@
 package jellyfish026.trunkhackathon
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class HomeScreen: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.home_screen)
+    }
+}
