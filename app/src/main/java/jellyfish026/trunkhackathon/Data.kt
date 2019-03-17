2687996c1fdc95b701a11b550c2f6b9c9a4ab0ce
jellyfish26 <jellyfish0162@gmail.com>
2019-03-17 14:58:09 +0900

    complete

--- app/src/main/java/jellyfish026/trunkhackathon/Data.kt
+++ app/src/main/java/jellyfish026/trunkhackathon/Data.kt
@@ -3,12 +3,18 @@ package jellyfish026.trunkhackathon
 object Data {
     var takoyakikun = arrayOf("さとし", "だれ", "え？")
     var HP = arrayOf(100, 100, 100)
-    var ATK = arrayOf(100, 100, 100)
-    var DEF = arrayOf(100, 100, 100)
-    var SPE = arrayOf(100, 100, 100)
+    var ATK = arrayOf(40, 40, 40)
+    var DEF = arrayOf(40, 40, 40)
+    var SPE = arrayOf(40, 40, 40)
     var EP = 0
     var TP = 0
     var selectTakoyaki = arrayOf(0, 0, 0);
     var nowID = 0;
+    var enemy_nowID = 0;
     var returnValue = 0;
+    var enemy_takoyakikun = arrayOf("enemy1", "enemy2", "enemy3")
+    var enemy_HP = arrayOf(120, 100, 100)
+    var enemy_ATK = arrayOf(40, 80, 60)
+    var enemy_DEF = arrayOf(30, 80, 40)
+    var enemy_SPE = arrayOf(50, 60, 30)
 }
